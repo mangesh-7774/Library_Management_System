b9901609dd4d69ef1ac94749995beaa01377b950
hani patil <hanipatil90@gmail.com>
2026-08-08 12:18:20 +0530

    Create library reports module

--- config/db_config.py
+++ config/db_config.py
@@ -1,6 +1,6 @@
 HOST = "localhost"
 USER = "root"
-PASSWORD = "mangesh@7774"      # Change according to your MySQL password
+PASSWORD = "hanipatil"      # Change according to your MySQL password
 DATABASE = "library_db"
 
 print("Database connected")
